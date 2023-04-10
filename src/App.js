524fc5678240655bc23855c61aa93644713ed74f
Marco Paiva <tulio.paiva10@gmail.com>
2023-04-10 18:19:45 -0300

    feat(start/screen: Working on questions and answers - game screen

--- src/App.js
+++ src/App.js
@@ -13,25 +13,42 @@ import GameScreen from './components/GameScreen/GameScreen';
 import GameOverScreen from './components/GameOverScreen/GameOverScreen';
 
 const stages = [
-  {id: 1, name: "start"},
-  {id: 2, name: "game"},
-  {id: 3, name: "end"},
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
 ];
 
 function App() {
   const startGame = () => {
     setGameStage(stages[1].name)
   }
+  const giveUp = () => {
+    setGameStage(stages[2].name)
 
-  const [gameStage, setGameStage] = useState(stages[1].name);
+  }
+
+  const aleatoryQuestion = () => {
+    const numberOfQuestions = questions.questions.length;
+    const aleatoryNum = Math.floor(Math.random() * numberOfQuestions);
+
+    return questions.questions[aleatoryNum];
+    // console.log(questions.questions[aleatoryNum].question_text);
+
+    // console.log(questions.questions[aleatoryNum].answers);
+
+  }
 
 
-  const theme = questions.questions[0].question_text;
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
+
+  // const question = questions.questions[1];
   return (
     <div className="App">
-      {gameStage =="start" && <StartScreen startGame={startGame}/>}
-      {gameStage =="game" && <GameScreen />}
-      {gameStage =="end" && <GameOverScreen />}
+      {gameStage == "start" && <StartScreen startGame={startGame} />}
+      {gameStage == "game" && <GameScreen giveUp={giveUp} question={aleatoryQuestion()}/>}
+      {gameStage == "end" && <GameOverScreen />}
 
     </div>
   );
--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,14 +1,49 @@
 import styles from './GameScreen.module.css';
 
 
-const GameScreen = () => {
+
+const GameScreen = ({ giveUp, question }) => {
+  console.log(question.answers);
   return (
     <div>
       <h1>Tela do Jogo</h1>
-      <div class="grid grid-cols-3 gap-4">
-        <div class="col">Column 1</div>
-        <div class="col">Column 2</div>
-        <div class="col">Column 3</div>
+      <div className={styles.container}>
+        <div className={styles.columnOne}>Coluna 1
+          <h2>
+            <span className={styles.show}>Show</span>
+            <span className={styles.do}>do</span>
+            <span className={styles.milhao}>Milhão</span>
+          </h2>
+          <h3>{question.question_text}</h3>
+          <p>1 {question.answers[0].answer_text}</p>
+          <p>2 {question.answers[1].answer_text}</p>
+          <p>3 {question.answers[2].answer_text}</p>
+          <p>4 {question.answers[3].answer_text}</p>
+
+          <div className={styles.container}>
+
+            <button onClick={giveUp}>Parar</button>
+            <button onClick={giveUp}>Confirma</button>
+
+          </div>
+          <div className={styles.containerThree}>
+            <div>
+              Card 1
+            <p>Errar</p>
+            </div>
+            <div>
+              Card 2
+            <p>Parar</p>
+            </div>
+            <div>
+              Card 3
+            <p>Acertar</p>
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.columnTwo}>Coluna 2</div>
+
       </div>
 
     </div>
